0a4f502aea474fcc3ffb07d7f4e587b2f40f5f9a
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-09 18:52:49 -0600

    Showing all employess with dept name and manager

--- Develop/index.js
+++ Develop/index.js
@@ -86,8 +86,11 @@ function viewRoles() {
 
 function viewEmployees() {
     db.query(`SELECT employee.id, employee.first_name, employee.last_name, 
-            role.title, role.salary  
+            role.title, department.name AS department, role.salary, 
+            CONCAT(manager.first_name," ",manager.last_name) AS manager
             FROM employee JOIN role ON employee.role_id = role.id
+            LEFT JOIN department ON role.department_id = department.id
+            LEFT JOIN employee manager ON employee.manager_id = manager.id;
             `, (err, results) => {
         if (err) throw err;
         // console.clear();
